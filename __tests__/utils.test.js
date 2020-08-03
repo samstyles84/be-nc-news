0a6f4484713d0eff8ce6fe1387cfc84afd2b3334
samstyles84 <sam.styles@arup.com>
2020-08-03 16:27:18 +0100

    completed formatting utils and seeding comments

--- __tests__/utils.test.js
+++ __tests__/utils.test.js
@@ -121,19 +121,160 @@ describe("formatDates", () => {
   });
 });
 
-describe("makeRefObj", () => {});
+describe("makeRefObj", () => {
+  test("Returns a new object", () => {
+    const input = [{ article_id: 1, title: "A" }];
+    const output = makeRefObj(input);
+    expect(typeof output).toBe("object");
+    expect(output).not.toBe(input);
+  });
+  test("Returns a reference for a single item", () => {
+    const input = [{ article_id: 1, title: "A" }];
+    const output = makeRefObj(input);
+    expect(output).toEqual({ A: 1 });
+  });
+  test("Returns a reference for multiple items", () => {
+    const input = [
+      { article_id: 1, title: "A" },
+      { article_id: 2, title: "B" },
+    ];
+    const output = makeRefObj(input);
+    expect(output).toEqual({ A: 1, B: 2 });
+  });
+  test("Does not mutate original", () => {
+    const input = [
+      { article_id: 1, title: "A" },
+      { article_id: 2, title: "B" },
+    ];
+    const output = makeRefObj(input);
+    expect(input).toEqual([
+      { article_id: 1, title: "A" },
+      { article_id: 2, title: "B" },
+    ]);
+  });
+});
 
 /*
 This utility function should be able to take an array (`list`) of objects and return a reference object. The reference object must be keyed by each item's title, with the values being each item's corresponding id. e.g.
-
 `[{ article_id: 1, title: 'A' }]`
-
 will become
-
 `{ A: 1 }`
 */
 
-describe("formatComments", () => {});
+describe("formatComments", () => {
+  test("Returns a new array", () => {
+    const inputComments = [
+      {
+        body:
+          "Oh, I've got compassion running out of my nose, pal! I'm the Sultan of Sentiment!",
+        belongs_to: "A",
+        created_by: "butter_bridge",
+        votes: 16,
+        created_at: 1511354163389,
+      },
+    ];
+    const articleRef = { A: 1 };
+    const output = formatComments(inputComments, articleRef);
+    expect(Array.isArray(output)).toBe(true);
+    expect(output).not.toBe(inputComments);
+    expect(output[0]).not.toBe(inputComments[0]);
+  });
+  test("Returns an array containing object with the correct properties", () => {
+    const inputComments = [
+      {
+        body:
+          "Oh, I've got compassion running out of my nose, pal! I'm the Sultan of Sentiment!",
+        belongs_to: "A",
+        created_by: "butter_bridge",
+        votes: 16,
+        created_at: 1511354163389,
+      },
+    ];
+    const articleRef = { A: 1 };
+    const output = formatComments(inputComments, articleRef);
+    expect("author" in output[0]).toBe(true);
+    expect("article_id" in output[0]).toBe(true);
+    expect(output[0].created_at).toBeInstanceOf(Date);
+    expect("article_id" in output[0]).toBe(true);
+    expect("created_by" in output[0]).toBe(false);
+    expect("belongs_to" in output[0]).toBe(false);
+    expect("body" in output[0]).toBe(true);
+    expect("votes" in output[0]).toBe(true);
+  });
+  test("Works on multiple comments", () => {
+    const inputComments = [
+      {
+        body:
+          "Oh, I've got compassion running out of my nose, pal! I'm the Sultan of Sentiment!",
+        belongs_to: "B",
+        created_by: "butter_bridge",
+        votes: 16,
+        created_at: 1511354163389,
+      },
+      {
+        body: "AAAAAA",
+        belongs_to: "A",
+        created_by: "alpha",
+        votes: 1,
+        created_at: 1411354163389,
+      },
+    ];
+    const articleRef = { A: 1, B: 2 };
+    const output = formatComments(inputComments, articleRef);
+
+    expect(output[0].article_id).toBe(2);
+    expect(output[1].article_id).toBe(1);
+    output.forEach((comment) => {
+      expect("author" in comment).toBe(true);
+      expect("article_id" in comment).toBe(true);
+      expect(comment.created_at).toBeInstanceOf(Date);
+      expect("article_id" in comment).toBe(true);
+      expect("created_by" in comment).toBe(false);
+      expect("belongs_to" in comment).toBe(false);
+      expect("body" in comment).toBe(true);
+      expect("votes" in comment).toBe(true);
+    });
+  });
+  test("Doesn't mutate original", () => {
+    const inputComments = [
+      {
+        body:
+          "Oh, I've got compassion running out of my nose, pal! I'm the Sultan of Sentiment!",
+        belongs_to: "B",
+        created_by: "butter_bridge",
+        votes: 16,
+        created_at: 1511354163389,
+      },
+      {
+        body: "AAAAAA",
+        belongs_to: "A",
+        created_by: "alpha",
+        votes: 1,
+        created_at: 1411354163389,
+      },
+    ];
+    const articleRef = { A: 1, B: 2 };
+    const output = formatComments(inputComments, articleRef);
+    expect(inputComments).toEqual([
+      {
+        body:
+          "Oh, I've got compassion running out of my nose, pal! I'm the Sultan of Sentiment!",
+        belongs_to: "B",
+        created_by: "butter_bridge",
+        votes: 16,
+        created_at: 1511354163389,
+      },
+      {
+        body: "AAAAAA",
+        belongs_to: "A",
+        created_by: "alpha",
+        votes: 1,
+        created_at: 1411354163389,
+      },
+    ]);
+    expect(articleRef).toEqual({ A: 1, B: 2 });
+  });
+});
 
 /*
 This utility function should be able to take an array of comment objects (`comments`) and a reference object, and return a new array of formatted comments.
--- db/utils/utils.js
+++ db/utils/utils.js
@@ -8,6 +8,23 @@ exports.formatDates = (list) => {
   return formattedArray;
 };
 
-exports.makeRefObj = (list) => {};
+exports.makeRefObj = (list) => {
+  const refObj = {};
+  list.forEach((item) => {
+    const val = item.article_id;
+    const key = item.title;
+    refObj[key] = val;
+  });
+  return refObj;
+};
 
-exports.formatComments = (comments, articleRef) => {};
+exports.formatComments = (comments, articleRef) => {
+  const formattedArray = exports.formatDates(comments);
+  formattedArray.forEach((comment) => {
+    comment.author = comment.created_by;
+    comment.article_id = articleRef[comment.belongs_to];
+    delete comment.created_by;
+    delete comment.belongs_to;
+  });
+  return formattedArray;
+};

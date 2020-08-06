9631720e70695e9306608cdc58645ed5ba0fbd81
samstyles84 <sam.styles@arup.com>
2020-08-06 15:13:11 +0100

    set up files for heroku server

--- db/data/index.js
+++ db/data/index.js
@@ -1,8 +1,11 @@
+//const data = { test, development, production: development };
 const testData = require("./test-data");
 const developmentdata = require("./development-data");
+const production = require("./development-data");
+
 const ENV = process.env.NODE_ENV || "development";
 
-if (ENV === "development") exportData = developmentdata;
+if (ENV !== "test") exportData = developmentdata;
 else exportData = testData;
 
 module.exports = exportData;
